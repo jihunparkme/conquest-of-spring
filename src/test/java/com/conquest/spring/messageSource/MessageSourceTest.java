26253474ae5572362def5241988fdf1a870f51dd
Jihun Park <jihunpark.tech@gmail.com>
2023-07-01 14:30:08 +0900

    [Conquest of Spring] 메시지 파일 선택

---
+++ src/test/java/com/conquest/spring/messageSource/MessageSourceTest.java
@@ -0,0 +1,50 @@
+package com.conquest.spring.messageSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        // locale 정보가 없으면 basename 에서 설정한 기본 이름 메시지 파일 조회
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        // 메시지가 없어도 defaultMessage 를 사용하면 기본 메시지 반환
+        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        // 메시지의 {0} 부분은 매개변수를 전달해서 치환
+        String result = ms.getMessage("hello.name", new Object[]{"Aaron"}, null);
+        assertThat(result).isEqualTo("안녕 Aaron");
+    }
+
+    @Test
+    void defaultLang() {
+        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+}
