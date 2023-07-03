22cd335fbe3c46e7ad1dd525bfa1e2e08e9a14b3
Jihun Park <jihunpark.tech@gmail.com>
2023-07-03 17:45:29 +0900

    [Conquest of Spring] DefaultFormattingConversionService

--- src/main/java/com/conquest/spring/type/conversion/WebConfig.java
+++ src/main/java/com/conquest/spring/type/conversion/WebConfig.java
@@ -1,9 +1,8 @@
 package com.conquest.spring.type.conversion;
 
-import com.conquest.spring.type.converter.IntegerToStringConverter;
 import com.conquest.spring.type.converter.IpPortToStringConverter;
-import com.conquest.spring.type.converter.StringToIntegerConverter;
 import com.conquest.spring.type.converter.StringToIpPortConverter;
+import com.conquest.spring.type.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
