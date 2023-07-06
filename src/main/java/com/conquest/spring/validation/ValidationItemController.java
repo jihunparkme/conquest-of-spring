37d6776a66878b8bfcfba90e36bd967ffe944f93
Jihun Park <jihunpark.tech@gmail.com>
2023-07-06 21:30:22 +0900

    [Conquest of Spring]  Validator

--- src/main/java/com/conquest/spring/validation/ValidationItemController.java
+++ src/main/java/com/conquest/spring/validation/ValidationItemController.java
@@ -157,4 +157,19 @@ public class ValidationItemController {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        itemValidator.validate(item, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 }
