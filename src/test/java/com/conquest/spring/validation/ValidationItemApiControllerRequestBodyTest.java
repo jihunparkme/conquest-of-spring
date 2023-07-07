718761cbd5bb9f5e2f169227a63207ac24ea3ae0
Jihun Park <jihunpark.tech@gmail.com>
2023-07-07 14:07:26 +0900

    [Conquest of Spring] test: RequestBody  API Validation Request

---
+++ src/test/java/com/conquest/spring/validation/ValidationItemApiControllerRequestBodyTest.java
@@ -0,0 +1,118 @@
+package com.conquest.spring.validation;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.conquest.spring.validation.domain.ItemSaveForm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ValidationItemApiControllerRequestBodyTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    private static final String BASE_URL = "/validation/api/items";
+
+    @Test
+    void validation_item_api_success() throws Exception {
+        String itemName = "hello";
+        int prive = 10000;
+        int quantity = 10;
+
+        ItemSaveForm request = ItemSaveForm.builder().itemName(itemName)
+                .price(prive)
+                .quantity(quantity)
+                .build();
+        String body = mapper.writeValueAsString(request);
+
+        String expected =
+                "{" +
+                    "\"itemName\":\"hello\"," +
+                    "\"price\":10000," +
+                    "\"quantity\":10" +
+                "}";
+        mvc.perform(post(BASE_URL + "/request-body/add")
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string(expected));
+    }
+
+    @Test
+    void validation_item_api_valudation_fail() throws Exception {
+        String itemName = "hello";
+        int prive = 1000;
+        int quantity = 100000;
+
+        ItemSaveForm request = ItemSaveForm.builder().itemName(itemName)
+                .price(prive)
+                .quantity(quantity)
+                .build();
+        String body = mapper.writeValueAsString(request);
+
+        String expected =
+                "[" +
+                    "{" +
+                        "\"codes\":[" +
+                            "\"Max.itemSaveForm.quantity\"," +
+                            "\"Max.quantity\"," +
+                            "\"Max.java.lang.Integer\"," +
+                            "\"Max\"" +
+                        "]," +
+                        "\"arguments\":[" +
+                        "{" +
+                            "\"codes\":[" +
+                                "\"itemSaveForm.quantity\"," +
+                                "\"quantity\"" +
+                            "]," +
+                            "\"arguments\":null," +
+                            "\"defaultMessage\":\"quantity\"," +
+                            "\"code\":\"quantity\"" +
+                        "}," +
+                        "9999" +
+                        "]," +
+                        "\"defaultMessage\":\"must be less than or equal to 9999\"," +
+                        "\"objectName\":\"itemSaveForm\"," +
+                        "\"field\":\"quantity\"," +
+                        "\"rejectedValue\":100000," +
+                        "\"bindingFailure\":false," +
+                        "\"code\":\"Max\"" +
+                    "}" +
+                "]";
+        mvc.perform(post(BASE_URL + "/request-body/add")
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string(expected));
+    }
+
+    @Test
+    void validation_item_api_request_fail() throws Exception {
+        String request =
+                "{" +
+                    "\"itemName\":\"hello\", " +
+                    "\"price\":\"A\", " +
+                    "\"quantit\": 10000" +
+                "}";
+
+        mvc.perform(post(BASE_URL + "/request-body/add")
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().is4xxClientError());
+    }
+}
