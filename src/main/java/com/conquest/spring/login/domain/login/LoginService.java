10ece2c7b34c202a6d1602d5b153cbdc167d932f
Jihun Park <jihunpark.tech@gmail.com>
2023-07-08 16:49:24 +0900

    [Conquest of Spring] init: login part

---
+++ src/main/java/com/conquest/spring/login/domain/login/LoginService.java
@@ -0,0 +1,18 @@
+package com.conquest.spring.login.domain.login;
+
+import com.conquest.spring.login.domain.member.Member;
+import com.conquest.spring.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
---
+++ src/main/java/com/conquest/spring/login/web/login/LoginController.java
@@ -0,0 +1,40 @@
+package com.conquest.spring.login.web.login;
+
+import com.conquest.spring.login.domain.login.LoginService;
+import com.conquest.spring.login.domain.member.Member;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리 TODO
+        return "redirect:/";
+    }
+}
