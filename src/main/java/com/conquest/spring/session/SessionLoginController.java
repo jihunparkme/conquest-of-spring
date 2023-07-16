279f2a7103fadaffef4ae2d5b746843542b7bf82
Jihun Park <jihunpark.tech@gmail.com>
2023-07-16 17:42:55 +0900

    [Conquest of Spring] refactor: add RequestMapping

--- src/main/java/com/conquest/spring/session/SessionLoginController.java
+++ src/main/java/com/conquest/spring/session/SessionLoginController.java
@@ -15,9 +15,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/session")
 @RequiredArgsConstructor
 public class SessionLoginController {
 
--- src/main/java/com/conquest/spring/session/httpSession/HttpSessionLoginController.java
+++ src/main/java/com/conquest/spring/session/httpSession/HttpSessionLoginController.java
@@ -11,13 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
+@RequestMapping("/http-session")
 @RequiredArgsConstructor
 public class HttpSessionLoginController {
 
