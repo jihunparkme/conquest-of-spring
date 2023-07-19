ad33a97c5464ad335ce524e4e52a28474ed077cc
Jihun Park <jihunpark.tech@gmail.com>
2023-07-19 22:22:05 +0900

    [Conquest of Spring] 필터와 Interceptor

--- src/main/java/com/conquest/spring/exception/servlet/dispatcherType/DispatcherTypeWebConfig.java
+++ src/main/java/com/conquest/spring/exception/servlet/dispatcherType/DispatcherTypeWebConfig.java
@@ -5,12 +5,30 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class DispatcherTypeWebConfig implements WebMvcConfigurer {
 
-    @Bean
+    /**
+     * 필터는 필터 등록 시 특정 DispatcherType 인 경우 필터가 적용되도록 설정이 가능했지만,
+     * 인터셉터는 스프링이 제공하는 기능이라서 DispatcherType 와 무관하게 항상 호출
+     *
+     * 대신 인터셉터의 excludePathPatterns 를 사용해서 특정 경로 제외 가능
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**", "/*.ico"
+                        , "/error", "/error-page/**" //오류 페이지 경로
+                );
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
