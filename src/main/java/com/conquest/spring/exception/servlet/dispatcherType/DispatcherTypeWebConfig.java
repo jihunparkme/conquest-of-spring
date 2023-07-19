081c27c882d43ba02cdd4f6f63e2a4a222d137f7
Jihun Park <jihunpark.tech@gmail.com>
2023-07-19 22:09:12 +0900

    [Conquest of Spring] 필터와 DispatcherType

---
+++ src/main/java/com/conquest/spring/exception/servlet/dispatcherType/DispatcherTypeWebConfig.java
@@ -0,0 +1,22 @@
+package com.conquest.spring.exception.servlet.dispatcherType;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DispatcherTypeWebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterRegistrationBean;
+    }
+}
