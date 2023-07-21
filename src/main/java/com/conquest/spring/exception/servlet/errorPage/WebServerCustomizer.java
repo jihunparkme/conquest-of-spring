84560ce90a8389aa3164a16f1124ee872d670a97
Jihun Park <jihunpark.tech@gmail.com>
2023-07-21 17:03:22 +0900

    [Conquest of Spring] API 예외 처리

--- src/main/java/com/conquest/spring/exception/servlet/errorPage/WebServerCustomizer.java
+++ src/main/java/com/conquest/spring/exception/servlet/errorPage/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component -> 스프링 부트가 제공하는 기본 오류 메커니즘을 사용하도록 컴포넌트 제외
+@Component //-> 스프링 부트가 제공하는 기본 오류 메커니즘을 사용하도록 컴포넌트 제외
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
