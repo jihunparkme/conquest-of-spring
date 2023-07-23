ddbb177912770498193c2cd11ae2607a754851c0
Jihun Park <jihunpark.tech@gmail.com>
2023-07-23 18:14:00 +0900

    [Conquest of Spring] ResponseStatusExceptionResolver

--- src/main/java/com/conquest/spring/exception/servlet/api/ApiExceptionController.java
+++ src/main/java/com/conquest/spring/exception/servlet/api/ApiExceptionController.java
@@ -1,11 +1,14 @@
 package com.conquest.spring.exception.servlet.api;
 
+import com.conquest.spring.exception.servlet.api.exceptionResolver.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -24,6 +27,16 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
     @Data
